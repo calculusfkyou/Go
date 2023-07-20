0b0aaac2cccf7652d224a96978ed5090fa81f034
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-21 00:04:47 +0800

    Initial commit

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	name := "Go Developers"
-	fmt.Println("Azure for", name)
+	// 創建一個整數指針，並分配內存並初始化為零值（即 int 的默認值 0）
+	numPtr := new(int)
+	fmt.Println(*numPtr) // Output: 0
 }
